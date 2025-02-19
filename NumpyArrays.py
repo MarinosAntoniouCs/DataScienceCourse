6c2915c3ed75b3b4f429c990ab2e6f3dde18c6a9
MarinosAntoniouCs <manton05@ucy.ac.cy>
2025-02-19 18:00:03 +0200

    Learning about numpy library

---
+++ NumpyArrays.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+l=[2,3,4,5] #A list
+print(l)
+print(type(l))
+print()
+arr=np.array(l) #Converting a list to a numpy array
+print(arr)
+print(type(arr))
+
+a=np.array([1,2,3,4]) #Numpy array has dimensions
+b=np.array([[1,2,3,4],[5,6,7,8]])
+print(a.ndim)
+print(b.ndim)
+print(b.shape) #Will give you the rows and then the collumns
+
+# Python Program illustrating
+# numpy.zeros method
+ 
+import numpy as geek
+ 
+b = geek.zeros(2, dtype = int) #One dimension of two columns
+print("Matrix b : \n", b)
+ 
+a = geek.zeros([2, 2], dtype = int) #Two dimensions of two columns
+print("\nMatrix a : \n", a)
+ 
+c = geek.ones([3, 3]) #Three dimensions with three columns
+print("\nMatrix c : \n", c)
+
+# output array
+out_arr = np.random.random(3) #np.random.random(columns) to give a one dimension array of #columns random columns
+print ("Output 1D Array filled with random floats : ", out_arr)
+
+print("A\n", np.arange(4).reshape(2, 2), "\n") #arange is like the range one , with the reshape you adjust rows/columns
+print("A\n", np.arange(4, 10), "\n")
+print("A\n", np.arange(4, 20, 3), "\n")#Third argument is for steps
